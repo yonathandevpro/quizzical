7c45bba3c913fccf34117cdd660ead2a31ce4c95
Yonathan Berhanu <yonathan.berhanu17@gmail.com>
2024-04-20 15:42:10 +0300

    Quiz component added

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import React, { useState } from 'react';
 import YellowShape from './assets/yellow-shape.png';
 import GrayShape from './assets/gray-shape.png';
 import StartPage from './components/StartPage';
-
+import Quizzes from './components/Quizzes';
 function App() {
       return (
           <div className="container">
              <img src={ YellowShape } className="yellow-background"/>
-             <StartPage />
+             <Quizzes />
+             <Quizzes />
+             <Quizzes />
+             <Quizzes />
+             <Quizzes />
+             <button className="check-answers">Check answers</button>
              <img src={ GrayShape } className="gray-background"/>   
           </div>  
       );  
---
+++ src/components/Quizzes.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+function Quizzes () {
+    return (
+        <div className="quiz">
+            <h1>How would one say goodbye in Spanish?</h1>
+            <div className="choices">
+                <button>Adios</button>
+                <button>Hola</button>
+                <button>Au Revoir</button>
+                <button>Salir</button>
+            </div>
+        </div>
+    );
+}
+
+export default Quizzes;
