778610d96e5981a8206767968b2a5c20ba40579e
Yonathan Berhanu <yonathan.berhanu17@gmail.com>
2024-04-20 13:40:24 +0300

    Build UI for start page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import React, { useState } from 'react'
-
-
+import React, { useState } from 'react';
+import YellowShape from './assets/yellow-shape.png';
+import GrayShape from './assets/gray-shape.png';
+import StartPage from './components/StartPage';
 
 function App() {
-
+      return (
+          <div className="container">
+             <img src={ YellowShape } className="yellow-background"/>
+             <StartPage />
+             <img src={ GrayShape } className="gray-background"/>   
+          </div>  
+      );  
 }
 
 export default App
---
+++ src/components/StartPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function StartPage () {
+    return (
+        <div className="start-page">
+            <h1>Quizzical</h1>
+            <h4>Test your knowledge with these awesome questions!</h4>
+            <button>Start Quiz</button>
+        </div>
+    );
+}
+
+export default StartPage;
