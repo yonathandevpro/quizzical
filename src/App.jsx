7db51d7fd7ec89ef79fa98c850bdf34601ecca78
Yonathan Berhanu <yonathan.berhanu17@gmail.com>
2024-04-20 18:26:10 +0300

    Integrated OTDB API to the App

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import YellowShape from './assets/yellow-shape.png';
 import GrayShape from './assets/gray-shape.png';
 import StartPage from './components/StartPage';
 import Quizzes from './components/Quizzes';
+import { nanoid } from 'nanoid'
+
+const API_URL = 'https://opentdb.com/api.php?amount=5';
 function App() {
+
+      const [ quizzes, setQuizzes ] = useState([]);
+
+      useEffect(() => {
+          async function getQuestions () {
+                const response = await fetch(API_URL);
+                const data = await response.json();
+                console.log(data);
+                if (data.response_code == 0) {
+                    setQuizzes(data.results);
+                }
+          }
+
+          getQuestions();
+      }, []);  
+
+      const questions = quizzes.map(quiz => {
+          const randomIndex = Math.floor(Math.random() * 4); 
+          const multipleChoices = [...quiz.incorrect_answers];
+          multipleChoices.splice(randomIndex, 0, quiz.correct_answer);
+          return <Quizzes 
+                    key={nanoid()}
+                    question={quiz.question}
+                    choices={multipleChoices}
+                  />  
+      });
+     
       return (
           <div className="container">
              <img src={ YellowShape } className="yellow-background"/>
-             <Quizzes />
-             <Quizzes />
-             <Quizzes />
-             <Quizzes />
-             <Quizzes />
+             <div className="box">
+                {questions}
+             </div>
              <button className="check-answers">Check answers</button>
              <img src={ GrayShape } className="gray-background"/>   
           </div>  
--- src/components/Quizzes.jsx
+++ src/components/Quizzes.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 
 
-function Quizzes () {
+function Quizzes (props) {
+    const multipleChoices = props.choices.map(choice => {
+        return <button dangerouslySetInnerHTML={{ __html: choice  }}/>
+    });
     return (
         <div className="quiz">
-            <h1>How would one say goodbye in Spanish?</h1>
+            <h1 dangerouslySetInnerHTML={{ __html: props.question  }} />
             <div className="choices">
-                <button>Adios</button>
-                <button>Hola</button>
-                <button>Au Revoir</button>
-                <button>Salir</button>
+                {multipleChoices}
             </div>
         </div>
     );
