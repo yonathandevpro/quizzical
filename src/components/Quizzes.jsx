88601684594a11b4bbe40118a788a42003c3da80
yonathandevpro <yonathanber@icloud.com>
2024-04-22 16:52:53 +0300

    Added styled buttons

--- src/components/Quizzes.jsx
+++ src/components/Quizzes.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function Quizzes({ id, question, choices, selected, onOptionSelect }) {
-    function handleButtonClick(choice) {
+  function handleButtonClick(choice) {
         onOptionSelect(id, choice);
     }
 
@@ -9,6 +9,8 @@ function Quizzes({ id, question, choices, selected, onOptionSelect }) {
         <button
             key={choice}
             value={choice}
+            onMouseEnter={(e) => { e.target.style.backgroundColor = '#D6DBF5'; }}
+            onMouseLeave={(e) => { e.target.style.backgroundColor = selected === choice ? '#D6DBF5' : '#F5F7FB'; }}
             onClick={() => handleButtonClick(choice)}
             style={selected === choice ? { backgroundColor: '#D6DBF5' } : { backgroundColor: '#F5F7FB' }}
             dangerouslySetInnerHTML={{ __html: choice }}
