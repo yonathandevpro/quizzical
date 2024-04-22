ac590b5e87ffa25288feb6bb6b0ca4d6fe0d1cbf
yonathandevpro <yonathanber@icloud.com>
2024-04-22 16:07:31 +0300

    Completed selection process

--- src/App.jsx
+++ src/App.jsx
@@ -3,51 +3,69 @@ import YellowShape from './assets/yellow-shape.png';
 import GrayShape from './assets/gray-shape.png';
 import StartPage from './components/StartPage';
 import Quizzes from './components/Quizzes';
-import { nanoid } from 'nanoid'
+import { nanoid } from 'nanoid';
 
 const API_URL = 'https://opentdb.com/api.php?amount=5';
+
 function App() {
+    const [quizzes, setQuizzes] = useState([]);
+    const [selected, setSelected] = useState({});
 
-      const [ quizzes, setQuizzes ] = useState([]);
-      
+    console.log(selected);
+    useEffect(() => {
+        async function getQuestions() {
+            const response = await fetch(API_URL);
+            const data = await response.json();
+            console.log(data);
+            if (data.response_code === 0) {
+                const shuffledQuizzes = data.results.map(quiz => {
+                    const randomIndex = Math.floor(Math.random() * 4); 
+                    const id = nanoid();
+                    const multipleChoices = [...quiz.incorrect_answers];
+                    multipleChoices.splice(randomIndex, 0, quiz.correct_answer);
+                    return {
+                        id: id,
+                        question: quiz.question,
+                        choices: multipleChoices
+                    };
+                });
+                setQuizzes(shuffledQuizzes);
+            }
+        }
 
-      useEffect(() => {
-          async function getQuestions () {
-                const response = await fetch(API_URL);
-                const data = await response.json();
-                console.log(data);
-                if (data.response_code == 0) {
-                    setQuizzes(data.results);
-                }
-          }
+        getQuestions();
+    }, []);
 
-          getQuestions();
-      }, []);  
+    const handleOptionSelect = (id, choice) => {
+        setSelected(prevSelected => ({
+            ...prevSelected,
+            [id]: choice
+        }));
+        
+    };
 
+    const questions = quizzes.map(quiz => (
+        <Quizzes 
+            key={quiz.id}
+            id={quiz.id}
+            question={quiz.question}
+            choices={quiz.choices}
+            selected={selected[quiz.id] || ''} // Pass down selected option for this quiz
+            onOptionSelect={handleOptionSelect}
+        />
+    ));
 
-      const questions = quizzes.map(quiz => {
-          const randomIndex = Math.floor(Math.random() * 4); 
-          const id = nanoid();
-          const multipleChoices = [...quiz.incorrect_answers];
-          multipleChoices.splice(randomIndex, 0, quiz.correct_answer);
-          return <Quizzes 
-                    key={id}
-                    id={id}
-                    question={quiz.question}
-                    choices={multipleChoices}
-                  />  
-      });
-     
-      return (
-          <div className="container">
-             <img src={ YellowShape } className="yellow-background"/>
-             <div className="box">
+  
+    return (
+        <div className="container">
+            <img src={YellowShape} className="yellow-background" alt="yellow shape" />
+            <div className="box">
                 {questions}
-             </div>
-             <button className="check-answers">Check answers</button>
-             <img src={ GrayShape } className="gray-background"/>   
-          </div>  
-      );  
+            </div>
+            {Object.keys(selected).length === 5 && <button className="check-answers">Check answers</button>}
+            <img src={GrayShape} className="gray-background" alt="gray shape" />
+        </div>
+    );  
 }
 
-export default App
+export default App;
--- src/components/Quizzes.jsx
+++ src/components/Quizzes.jsx
@@ -1,53 +1,23 @@
-import React, {useState} from "react";
-import { nanoid } from 'nanoid';
+import React from 'react';
 
-function Quizzes (props) {
-
-    
-
-    const [ selected, setSelected ] = useState([]);
-
-    function findSelected (id, choice) {
-        const index = selected.findIndex(answer => answer.id === id);
-        if (index !== -1 && selected[index].selectedOption === choice) {
-            return { backgroundColor: '#D6DBF5' };
-        } else {
-            return { backgroundColor: '#F5F7FB' };
-        }
+function Quizzes({ id, question, choices, selected, onOptionSelect }) {
+    function handleButtonClick(choice) {
+        onOptionSelect(id, choice);
     }
 
-    function handleButtonClick (event, id) {
-        const choice = event.target.value;
-
-        const index = selected.findIndex(answer => answer.id === id);
-
-        if (index !== -1) {
-            setSelected (prevState => {
-                const updatedSelected = [...prevState];
-                updatedSelected[index] = {id: id, selectedOption: choice}
-                return updatedSelected;
-            });
-        } else {
-            setSelected(prevState => [...prevState, {id: id, selectedOption: choice}]);
-        }
-    }
-
-
-    const multipleChoices = props.choices.map(choice => {
-        const identifier = nanoid();
-        return <button 
-                    key={identifier}
-                    id={props.id}
-                    value={choice}
-                    onClick={(event) => handleButtonClick(event, props.id)} 
-                    style={findSelected(props.id, choice)} 
-                    dangerouslySetInnerHTML={{ __html: choice  }}
-                />
-    });
+    const multipleChoices = choices.map(choice => (
+        <button
+            key={choice}
+            value={choice}
+            onClick={() => handleButtonClick(choice)}
+            style={selected === choice ? { backgroundColor: '#D6DBF5' } : { backgroundColor: '#F5F7FB' }}
+            dangerouslySetInnerHTML={{ __html: choice }}
+        />
+    ));
 
     return (
         <div className="quiz">
-            <h1 dangerouslySetInnerHTML={{ __html: props.question  }} />
+            <h1 dangerouslySetInnerHTML={{ __html: question }} />
             <div className="choices">
                 {multipleChoices}
             </div>
